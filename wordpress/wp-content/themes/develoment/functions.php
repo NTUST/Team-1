52c7097011abcc046072736a07197897d522756a
kjj6198 <kjj6198@gmail.com>
2015-04-13 09:44:44 +0800

    update README

---
+++ wordpress/wp-content/themes/develoment/functions.php
@@ -0,0 +1,4 @@
+<?php
+register_nav_menus( array(
+	'primary-menu' => '主選單',
+) ); ?>
---
+++ wordpress/wp-content/themes/develoment/sidebar.php
@@ -0,0 +1,21 @@
+<div class="col_3">
+	<p>熱門文章</p>
+	<ul class="side-container">
+		<a href=""><li>
+			<p>失敗是放棄生存下去的勇氣</p>
+		</li></a>
+		<a href=""><li>
+			<p>失敗是放棄生存下去的勇氣</p>
+		</li></a>
+		<a href="">				<li>
+			<p>失敗是放棄生存下去的勇氣</p>
+		</li></a>
+		<a href=""><li>
+			<p>失敗是放棄生存下去的勇氣</p>
+		</li></a>
+		
+		
+		
+	</ul>
+	
+</div>

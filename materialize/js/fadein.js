13b62a39220a5cca729f37c5216dba41a6b8270a
kjj6198 <kjj6198@gmail.com>
2015-06-02 11:37:17 +0800

    latest version update the profile

---
+++ materialize/js/fadein.js
@@ -0,0 +1,18 @@
+$(function(){
+   $("#open-menu").click(function(){
+      $(".l-menu").toggleClass(function(){
+          if($(this).is(".fadeInLeft")){
+              $(this).removeClass("fadeInLeft");
+              $(".main-container").addClass("close-menu");
+              return "fadeOutLeft";
+          }
+          else{
+              $(this).removeClass("fadeOutLeft");
+              $(this).removeClass("hide-on-small-only");
+              $(".main-container").removeClass("close-menu");
+              return "fadeInLeft";
+          }
+      })
+
+   })
+});

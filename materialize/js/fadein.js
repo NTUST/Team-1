caaf2ececd093b0e1bd704999f7f3fb6372831cd
kjj6198 <kjj6198@gmail.com>
2015-06-05 14:26:14 +0800

    微調細部功能，加入內容及RWD

--- materialize/js/fadein.js
+++ materialize/js/fadein.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+    if($(".full-page").width() > 300){
+      $("l-menu").addClass('hide-on-small-only');
+      $("l-menu").removeClass('fadeOutLeft');
+    }
+  
+});
 $(function(){
    $("#open-menu").click(function(){
       $(".l-menu").toggleClass(function(){
@@ -12,6 +19,7 @@ $(function(){
               $(".main-container").removeClass("close-menu");
               return "fadeInLeft";
           }
+          //如果螢幕調回993px以上則將動畫效果刪除，改為固定顯示
       })
 
    })
